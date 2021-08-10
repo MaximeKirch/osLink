9297d2cb662757716113aceb53aed0e533544fae
Fabrice <77060087+FabriceH99@users.noreply.github.com>
2021-08-10 22:44:00 +0200

    Création composant NousDev et Footer
    Mise en forme non finie

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import Body from './components/Body'
 import './App.css';
 import HeroBanner from'./components/HeroBanner'
 import ContactForm from './components/ContactForm'
+import NousDev from './components/NousDev'
 import Twitter from './components/Twitter'
+import Footer from './components/Footer'
 
 
 function App() {
   return (
     <div className="App">
+      <div className="appContent">
       <header className="appHeader">
         <Nav />
         
       </header>
 
       <HeroBanner />
+      <NousDev />
       <Body />
       <ContactForm />
-      
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import '../App.css'
+
+
+function Footer() {
+    return(
+    <>
+    <div className="footerMain">
+        <div className="footerContainer">
+            <div className="footerRow">
+                {/*colonne1*/}
+                <div className="col">
+                    <h3> Où Nous trouver ?</h3>
+                    <ul className="footerList">
+                        <li>Fajema</li>
+                        <li>Partout dans le monde</li>
+                        <li>Nous sommes en Remote</li>
+                    </ul>
+                </div>
+                {/*colonne2*/}
+                <div className="col">
+                    <h3>Créé par</h3>
+                    <ul className="footerList">
+                        <li>Fabrice HUET</li>
+                        <li>Jefferson Guegan</li>
+                        <li>Maxime Kirch</li>
+                    </ul>
+                </div>
+                {/*colonne3*/}
+                <div className="col">
+                <h3>Sur les réseaux</h3>
+                    <ul className="footerList">
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Facebook</li>
+                    </ul>
+                </div>    
+            </div>
+                     
+        </div>
+        <hr></hr>
+        <div className="footerNoRow">
+            <p className="lastFooter">
+            &copy;{new Date().getFullYear()} FAJEMA INC</p>
+            <p className="lastFooter">
+            <a href="#">Engagement de confidentialité</a> | <a href="#">Utilisation des cookies</a> | <a href="#">Conditions d’utilisation</a> | <a href="#">Mentions légales</a> | <a href="#">Plan du site</a>
+            </p>   
+            </div> 
+    </div>
+        
+
+
+</>
+    )
+}
+
+export default Footer
