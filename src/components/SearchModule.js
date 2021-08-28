4991a52a6bdca0c7ef8b50b871f8bc86b08d756c
Jefferson <jefferson.guegan@outlook.fr>
2021-08-28 16:20:17 +0200

    Amélioration CSS module recherche

--- src/components/SearchModule.js
+++ src/components/SearchModule.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import fork from '../assets/fork.png'
+import star from '../assets/star.png'
+import gitstars from '../assets/gitstars.png'
 
 const SearchModule = (props) => {
 
@@ -9,25 +12,36 @@ const SearchModule = (props) => {
     
     return (
         <div className="searchModule">
-            <div className="searchCard">
-                <div className="data-container">
-                    <a href={project.url}><img src={project.avatar} alt="avatar" /></a>
-                    <ul>
-                        <li>{project.name}</li>
-                        <li>{project.author}</li>
-                        <li>{project.language}</li>
-                        <li>{project.description}</li>   
-                    </ul>
-                <div className="cardFooter">
-                    <ul>
-                        <li>{project.stars}</li>
-                        <li>{project.forks}</li>
-                        
-                    </ul>
-                </div>
 
-                </div>
-            </div>
+
+                <li className="searchCard">
+                    <a href={project.url}><img src={project.avatar} alt="avatar" /></a>
+
+                    <div className="dataContainer">
+
+                        <div className="mainCard">
+
+                            <ul>
+
+                                <li id="nameProject">{project.name}</li>
+                                <li>{project.author}</li>
+                                <li>{project.language}</li>
+                                <li id="descriptionProject">{project.description}</li>   
+                            </ul>
+
+                        </div>
+
+                        <div className="footerCard">
+                            <ul>
+                                <li className="starsProject"><img src={gitstars} id='starProject' alt='stars'/>{project.stars} stars</li>
+                                <li className="forksProject"><img src={fork} id="forkProject" alt='fork'/>{project.forks}</li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                </li>
+
         </div>
 
     );
