5fe82b4e77d7faa499eda83562a5afe32614fd8e
Fabrice HUET <fhuet99@gmail.com>
2021-11-02 20:06:22 +0100

    update.css

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -3,31 +3,33 @@ import '../App.css'
 
 export default function Partners() {
     return (
-        <div className='partnerBody'>
-                <h2 id='partnerTitle'>Ils nous font confiance : </h2>
+        <div className="partnerContainer">
+            <div className='partnerBody'>
+                    <h2 id='partnerTitle'>Ils nous font confiance : </h2>
 
-                <div className='partnerMain'>
+                    <div className='partnerMain'>
 
-                    <div className='partnerInfo'>
-                        <a href='https://www.wildcodeschool.com' target='_blank' rel='noreferrer'><img id='WCS' src="https://avatars.githubusercontent.com/u/8874047?s=280&v=4" alt='Wild Code School logo' />
-                        <h3>Wild Code School</h3></a>
+                        <div className='partnerInfo'>
+                            <a href='https://www.wildcodeschool.com' target='_blank' rel='noreferrer'><img id='WCS' src="https://avatars.githubusercontent.com/u/8874047?s=280&v=4" alt='Wild Code School logo' />
+                            <h3>Wild Code School</h3></a>
 
-                    </div>
+                        </div>
 
-                    <div className='partnerInfo'>
-                        <a href='https://www.github.com' target='_blank' rel='noreferrer'><img id='git' src='https://github.githubassets.com/images/modules/logos_page/Octocat.png' alt='git' />
-                        <h3>GithubTrends</h3> </a>
-                    </div>
+                        <div className='partnerInfo'>
+                            <a href='https://www.github.com' target='_blank' rel='noreferrer'><img id='git' src='https://github.githubassets.com/images/modules/logos_page/Octocat.png' alt='git' />
+                            <h3>GithubTrends</h3> </a>
+                        </div>
 
-                    <div className='partnerInfo'>
-                        <a href='https://www.google.fr' target='_blank' rel='noreferrer'><img id='google' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png" alt='Google Logo'/>
-                        <h3>Google</h3> </a>
-                    </div>
+                        <div className='partnerInfo'>
+                            <a href='https://www.google.fr' target='_blank' rel='noreferrer'><img id='google' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png" alt='Google Logo'/>
+                            <h3>Google</h3> </a>
+                        </div>
 
-            
                 
-                </div>
-            
+                    
+                    </div>
+                
+            </div>
         </div>
     )
 }
